6b9cff775201549b64e6698b1ee6872e190e1a3e
Leandro Estrella <trialsgama@gmail.com>
2018-11-04 14:31:38 -0300

    Updating changes

--- url_to_ip/routes/index.js
+++ url_to_ip/routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var body = require("body-parser");
 var router = express.Router();
 var net = require('net');
 const dns = require('dns');
@@ -7,17 +8,34 @@ router.get('/', function(req, res, next) {
     res.render('Buscar_url', { title: 'Url to IP' });
 });
 
-router.post('/', function(req, next) {
-    var dominio = req.body.url;
+router.post('/', function(req, res, next) {
+    var dominio = req.body.urlabuscar;
+    console.log(dominio);
+    var family = 4;
     var protocolo = req.body.protocolo;
-    var ip = dns.lookup(dominio);
+    console.log(protocolo);
+    dns.lookup(dominio, family, (err, address) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('%j', address);
+    });
+
+    var ip = dns.lookup("novared.com.ar", family, (err, address) => {
+        if (err) {
+            console.log(err);
+        }
+        return address;
+    });
+
+    console.log(ip);
     var client = new net.Socket();
     var conexion_servidor = client.connect(protocolo, ip, function() {
-        console.log(conexion_servidor);
+        return conexion_servidor;
     });
     console.log(conexion_servidor);
 
-
+    res.redirect('/');
 
 });
 
