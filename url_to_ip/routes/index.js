a175cb2eefc7de596b28f81ce0a1f5399990c1da
trialsgama <trialsgama@gmail.com>
2018-11-04 19:44:00 -0300

    Probando libreria

--- url_to_ip/routes/index.js
+++ url_to_ip/routes/index.js
@@ -3,40 +3,54 @@ var body = require("body-parser");
 var router = express.Router();
 var net = require('net');
 const dns = require('dns');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('Buscar_url', { title: 'Url to IP' });
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', function(req,res, next) {
     var dominio = req.body.urlabuscar;
     console.log(dominio);
     var family = 4;
     var protocolo = req.body.protocolo;
+    var datos = "";
     console.log(protocolo);
-    dns.lookup(dominio, family, (err, address) => {
+   
+   dns.lookup(dominio, family, (err, address) => {
         if (err) {
             console.log(err);
         }
         console.log('%j', address);
-    });
+        var client = new net.Socket();
+        client.connect(protocolo, address, function(err) {
+            if (err) {
+                console.log(err);
+            }
+             datos = client.setEncoding();
+        });
+        console.log(datos);
 
-    var ip = dns.lookup("novared.com.ar", family, (err, address) => {
+    });
+   
+  /*
+    var ip = dns.lookup(dominio, family, (err, address) => {
         if (err) {
             console.log(err);
         }
         return address;
     });
-
-    console.log(ip);
+    */
+    
+    /*
     var client = new net.Socket();
     var conexion_servidor = client.connect(protocolo, ip, function() {
         return conexion_servidor;
     });
     console.log(conexion_servidor);
 
-    res.redirect('/');
-
+    res.write(req.body.respuesta);
+    */
 });
 
 module.exports = router;
